6c2dd60e8a721a4177bb1332662e8725249be50b
Omer Hershkoviz <omerh86@gmail.com>
2017-11-19 11:11:26 +0200

    init commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { ConfigToolService } from './configuration-tool/config-tool-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ConfigToolService]
 })
 export class AppComponent {
   title = 'app';
+  jsonToShow: string = "";
+
+  constructor(private _configToolService: ConfigToolService) {
+
+  }
+
+  onWorkType() {
+    this.jsonToShow = this._configToolService.getWorkType();
+    console.log(this.jsonToShow);
+  }
+
+  onForms() {
+    this.jsonToShow = this._configToolService.getForms();
+    console.log(this.jsonToShow);
+  }
+
+  onSections() {
+    this.jsonToShow = this._configToolService.getSections();
+    console.log(this.jsonToShow);
+  }
+
+  onSectionToForms() {
+    this.jsonToShow = this._configToolService.getSectionToForms();
+    console.log(this.jsonToShow);
+  }
+
+  onFields() {
+    this.jsonToShow = this._configToolService.getFields();
+    console.log(this.jsonToShow);
+  }
+
+  onOptions() {
+    this.jsonToShow = this._configToolService.getOptions();
+    console.log(this.jsonToShow);
+  }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+     ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        NgbModule.forRoot(),
+        CommonModule
+        ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
